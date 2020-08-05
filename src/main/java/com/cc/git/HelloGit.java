43dce59110a51286bc59553014869a52bb03769d
wormcjc <49836432+wormcjc@users.noreply.github.com>
2020-08-05 16:13:40 +0800

    在线更新

--- src/main/java/com/cc/git/HelloGit.java
+++ src/main/java/com/cc/git/HelloGit.java
@@ -3,10 +3,11 @@ package com.cc.git;
 public class HelloGit {
 public static void main(String[] args) {
 	System.out.println("hello Git!");
-	System.out.println("更新1");
-	System.out.println("创建分支");
-	System.out.println("主干添加");
-	System.out.println("分支添加");
+	System.out.println("鏇存柊1");
+	System.out.println("鍒涘缓鍒嗘敮");
+	System.out.println("涓诲共娣诲姞");
+	System.out.println("鍒嗘敮娣诲姞");
+	System.out.println("鍒嗘敮娣诲姞");//鍦ㄧ嚎缂栬緫
 
 }
 }
